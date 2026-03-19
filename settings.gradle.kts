4be14ec68a3f69ca88d45580e2bd3ffb6cd75cbf
viktorashi <ioanvictorstan@gmail.com>
2026-03-19 23:00:36 +0200

    Fix Gradle Java 25 compatibility

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 rootProject.name = "cs-ubb-timetable-parser"
-
